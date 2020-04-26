9fb178fd86cf374b9d96f2fcf763e8eb8427b24b
giacomo <giacomo.petrucci@saep-ict.it>
2020-04-26 10:42:13 +0200

    depl10

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-docker build -t giacomopetru/multi-client:latest -t giacomopetru/multi-client$SHA -f ./client/Dockerfile ./client
-docker build -t giacomopetru/multi-server:latest -t giacomopetru/multi-server$SHA -f ./server/Dockerfile ./server
-docker build -t giacomopetru/multi-worker:latest -t giacomopetru/multi-worker$SHA -f ./worker/Dockerfile ./worker
+
+docker build -t giacomopetru/multi-client:latest -t giacomopetru/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t giacomopetru/multi-server:latest -t giacomopetru/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t giacomopetru/multi-worker:latest -t giacomopetru/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push giacomopetru/multi-client:latest
 docker push giacomopetru/multi-server:latest
@@ -11,6 +12,6 @@ docker push giacomopetru/multi-server:$SHA
 docker push giacomopetru/multi-worker:$SHA
 
 kubectl apply -f k8sBis
-kubectl set image deployments/server-deployments server=giacomopetru/multi-server:$SHA
-kubectl set image deployments/client-deployments client=giacomopetru/multi-client:$SHA
-kubectl set image deployments/worker-deployments worker=giacomopetru/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=giacomopetru/multi-server:$SHA
+kubectl set image deployments/client-deployment client=giacomopetru/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=giacomopetru/multi-worker:$SHA
